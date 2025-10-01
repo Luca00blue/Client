55bfdc4e2bea0c76065da224770e7110eb8714ef
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 09:50:19 +0200

    connessione

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,22 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("Hello world!");
+        System.out.println("inserisci");
+        
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter ip");
+
+        String ip = myObj.nextLine();
+        System.out.println("inserisci porta del server");
+        int porta = myObj.nextInt();
+        Socket miSocket = new Socket(ip,porta);
+
     }
 }
